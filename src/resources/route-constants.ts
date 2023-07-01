74f952ec5abf70dcf046cb5c5debbfc8941a8bf6
Daniel Alexander Panczyk <daniel.alexander.panczyk@mni.thm.de>
2023-07-01 18:11:25 +0200

    Added Bewertung Abgeben

--- src/resources/route-constants.ts
+++ src/resources/route-constants.ts
@@ -4,6 +4,7 @@ export const ROUTES = {
   ANGEBOTE_ROUTE: '/angebote',
   GESUCHE_ROUTE: '/gesuche',
   BEWERTUNGEN_ROUTE: '/bewertungen',
+  BEWERTUNG_ABGEBEN_ROUTE: '/bewertung-abgeben',
   FAHRTEN_DETAILANSICHT_ROUTE: '/fahrt-detailansicht',
   OTHER_PROFILEPAGE_ROUTE: '/profil/:id',
   FAHRTEN_ANLAGE_ROUTE: '/fahrten_anlage',
