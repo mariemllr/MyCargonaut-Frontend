b2cc995614213222120019931b6b7c30e5de68f7
Daniel Alexander Panczyk <daniel.alexander.panczyk@mni.thm.de>
2023-09-18 11:47:52 +0200

    Chat now works with multiple people

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -6,6 +6,8 @@ import {
   TextField,
   Paper,
   Button,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { color, styled } from "@mui/system";
 import React, { useState } from "react";
@@ -23,42 +25,72 @@ interface Message {
   sender: boolean;
 }
 
+interface ChatPartner {
+  id: number;
+  name: string;
+  messages: Message[];
+}
+
 const Chat: React.FC = () => {
   const [input, setInput] = useState<string>("");
-  const [messages, setMessages] = useState<Message[]>([
-    // Beispieltext kann Entfernt werden sobald die Verbindung zum Backend steht
+  const [currentPartner, setCurrentPartner] = useState<number>(0);
+  const [chatPartners, setChatPartners] = useState<ChatPartner[]>([
     {
       id: 0,
-      text: "Hallo, kann ich mit dir nach Dresden fahren",
-      sender: false,
+      name: "Martina",
+      messages: [
+        {
+          id: 0,
+          text: "Hallo, kann ich mit dir nach Dresden fahren",
+          sender: false,
+        },
+        {
+          id: 1,
+          text: "Hallo! klar, was möchtest du noch mitnehmen?",
+          sender: true,
+        },
+      ],
     },
     {
       id: 1,
-      text: "Hallo! klar, was möchtest du noch mitnehmen?",
-      sender: true,
-    },
-    {
-      id: 2,
-      text: "Super, ein Klavier, ein Hund, ein Fharrad ...",
-      sender: false,
+      name: "Axel",
+      messages: [
+        {
+          id: 0,
+          text: "Wie geht es dir heute?",
+          sender: false,
+        },
+      ],
     },
   ]);
 
   const handleMessageSend = () => {
     const newMessage: Message = {
-      id: messages.length,
+      id: chatPartners[currentPartner].messages.length,
       text: input,
       sender: true,
     };
-    setMessages([...messages, newMessage]);
+    const updatedPartners = [...chatPartners];
+    updatedPartners[currentPartner].messages.push(newMessage);
+    setChatPartners(updatedPartners);
     setInput("");
   };
 
   return (
     <Container maxWidth="sm">
       <h1>Chat</h1>
+      <Select
+        value={currentPartner}
+        onChange={(e) => setCurrentPartner(e.target.value as number)}
+        fullWidth>
+        {chatPartners.map((partner) => (
+          <MenuItem key={partner.id} value={partner.id}>
+            {partner.name}
+          </MenuItem>
+        ))}
+      </Select>
       <List>
-        {messages.map((message) => (
+        {chatPartners[currentPartner].messages.map((message) => (
           <ListItem key={message.id} style={{ padding: 0 }}>
             <div
               style={{
